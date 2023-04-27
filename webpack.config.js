f035d9ae0a69e9102d312b1fad29a57c6505c494
zhangsan <lxs2048@sina.com>
2023-04-28 00:36:18 +0800

    配置webpack实现热更新

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js',
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,//指定什么扩展类型使用以下加载器
+                exclude: /node_modules/,//排除
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', { 'runtime': 'automatic' }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.svg$/i,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'popup.html',
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "public",
+                },
+            ],
+        }),
+    ],
+};
